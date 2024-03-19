5f70078b1a2f106542fd769c37e1ff0c0b82a352
tvaic <t.vaicekauskas@gmx.com>
2024-03-19 11:31:15 +0000

    Testing existing job reads

--- __main__.py
+++ __main__.py
@@ -6,7 +6,14 @@ cron = CronTab(user=True)
 # List all existing cron jobs
 print("Existing cron jobs:")
 for job in cron:
+    print('=' * len(job))
     print(job)
+    print(f"\tMinutes:\t{job.minutes}")
+    print(f"\tHour:\t{job.Hours}")
+    print(f"\tDay:\t{job.days_of_month}")
+    print(f"\tMonth:\t{job.months}")
+    print(f"\tDow:\t{job.dows}")
+    print(f"\tCommand:\t{job.command}")
 
 # # Add a new cron job
 # new_job = cron.new(command='/path/to/your/command', comment='Your comment here')
