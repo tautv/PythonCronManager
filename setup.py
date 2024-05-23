5f2ab4565a295e1143571c372cfa2235eedf201a
tautv <T.Vaicekauskas@gmx.com>
2024-05-23 19:44:34 +0100

    Win build fixup

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 import sys
+import os
+
+# Create necessary directories if they don't exist
+dist_dir = 'dist/win' if sys.platform.startswith('win') else 'dist/mac'
+if not os.path.exists(dist_dir):
+    os.makedirs(dist_dir)
 
 # Common dependencies
 common_deps = [
@@ -9,18 +15,21 @@ common_deps = [
 
 # Windows-specific setup
 if sys.platform.startswith('win'):
+    import py2exe
     extra_deps = ['py2exe']
     options = {
         'py2exe': {
             'packages': ['wx'],  # Include wxPython package
             'dist_dir': 'dist/win',  # specify the output directory as dist/win for py2exe
-            'icon_resources': [1, 'src/images/icon.ico']
         }
     }
     setup(
         name='PythonCronManager',
-        version='0.01',
-        windows=[{'script': 'src/main.py'}],
+        version='0.1.0',
+        windows=[{
+            'script': 'src/main.py',
+            'icon_resources': [(1, 'src/images/icon.ico')]
+        }],
         options=options,
         install_requires=common_deps + extra_deps,
     )
@@ -37,7 +46,7 @@ elif sys.platform.startswith('darwin'):
     }
     setup(
         name='PythonCronManager',
-        version='0.01',
+        version='0.0.1',
         app=['src/main.py'],
         options=options,
         install_requires=common_deps + extra_deps,
