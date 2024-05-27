8742bdd20661c430ddeebb91579541a46391551a
tautv <T.Vaicekauskas@gmx.com>
2024-05-27 20:56:39 +0100

    spell

--- scripts/tree_printer.py
+++ scripts/tree_printer.py
@@ -23,30 +23,3 @@ if __name__ == '__main__':
     directory_path = '.'
     print(f"Directory Tree of {directory_path}:")
     print_directory_tree(directory_path)
-
-
-# |-- requirements.txt
-# |-- tests
-#     |-- __init__.py
-#     |-- utils
-#         |-- __init__.py
-#         |-- test_split_cron_command.py
-#         |-- test_join_cron_command.py
-# |-- README.md
-# |-- .gitignore
-# |-- scripts
-#     |-- run_tests.sh
-# |-- src
-#     |-- __init__.py
-#     |-- utils
-#         |-- cron_command_parse.py
-#         |-- __init__.py
-#         |-- cron_manager.py
-#         |-- TreePrinter.py
-#     |-- setup.py
-#     |-- gui
-#         |-- launch.py
-#         |-- frames.py
-#         |-- DataViewModels.py
-#         |-- dialogs.py
-#     |-- main.py
