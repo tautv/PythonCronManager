3c9cee49466f34992ec407f5df78cce81a289941
tvaic <t.vaicekauskas@gmx.com>
2024-05-30 08:59:24 +0100

    Fixed custom event template

--- src/ui/custom_events/custom_event.py
+++ src/ui/custom_events/custom_event.py
@@ -1,32 +1,45 @@
 import wx
 
 # Define a new event type
-myEVT_BUTTON_CLICKED = wx.NewEventType()
-EVT_BUTTON_CLICKED = wx.PyEventBinder(myEVT_BUTTON_CLICKED, 1)
+myEVT_Custom_Event = wx.NewEventType()
+EVT_CUSTOM_EVENT = wx.PyEventBinder(myEVT_Custom_Event)
 
 
 class ButtonClickedEvent(wx.PyCommandEvent):
     def __init__(self, event_type, id):
-        wx.PyCommandEvent.__init__(self, event_type, id)
+        super().__init__(event_type, id)
         self.data = None
 
     def GetData(self):
         return self.data
 
+    def SetData(self, data):
+        self.data = data
 
-# import wx
-# from .custom_events.py import ButtonClickedEvent, myEVT_BUTTON_CLICKED
+
+# if __name__ == "__main__":
+#     class MyFrame(wx.Frame):
+#         def __init__(self):
+#             super(MyFrame, self).__init__(None, title="MyFrame", size=(200, 300))
 #
-# class MyPanel(wx.Panel):
-#     def __init__(self, parent):
-#         super(MyPanel, self).__init__(parent)
+#             self.button = wx.Button(self, label="Click Me")
+#             self.Bind(wx.EVT_BUTTON, self.OnButtonClicked, self.button)
+#             self.Bind(EVT_CUSTOM_EVENT, self.OnCustomButtonClicked)
+#             self.Show()
 #
-#         self.button = wx.Button(self, label="Click Me")
-#         self.Bind(wx.EVT_BUTTON, self.OnButtonClicked, self.button)
+#         def OnButtonClicked(self, event):
+#             # Create a custom event
+#             evt = ButtonClickedEvent(myEVT_Custom_Event, self.GetId())
+#             evt.SetData("Some relevant data")
+#             # Post the event to be handled by the main application or controller
+#             wx.PostEvent(self, evt)
 #
-#     def OnButtonClicked(self, event):
-#         # Create a custom event
-#         evt = ButtonClickedEvent(myEVT_BUTTON_CLICKED, self.GetId())
-#         evt.SetData("Some relevant data")
-#         # Post the event to be handled by the main application or controller
-#         wx.PostEvent(self.GetParent(), evt)
+#         def OnCustomButtonClicked(self, event):
+#             # Handle the custom event
+#             data = event.GetData()
+#             print(f"myEVT_Custom_Event data: {data}")
+#
+#
+#     app = wx.App(False)
+#     frame = MyFrame()
+#     app.MainLoop()
